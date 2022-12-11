c244bea499a872e2199cd45b44a56120231bfabe
kneerose <duwalkneerose@gmail.com>
2022-12-11 15:56:11 +0545

    Add covid19 source code

---
+++ lib/themes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color barcolor = Color(0xff24405c);
+Color bgcolor = Colors.white;
+Color darkmodeba = Colors.blueGrey;
+Color darkmodeb = Color(0xa9a9a9);
+Color textcolor = Colors.black;
+Color buttoncolor = Color(0xff24405c);
